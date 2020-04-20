c2e1bd4d5c717a4725a5cb6764db968083b578f3
AhmedElaguab <elaguabahmed97@gmail.com>
2020-04-20 21:17:18 +0100

    Initialize project using Create React App

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+const App = () => {
+  return <div className="app">App</div>
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
